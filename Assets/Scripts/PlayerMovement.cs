6fdaef7425d9b1f1df7a81f708a4787f1621721b
Thomas <verhile.tomas@orange.fr>
2022-02-04 17:08:30 +0100

    fixed room and player prefab + player movement adjustement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -32,8 +32,8 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move()
     {
-        Vector2 newPosition = new Vector2(horizontalInput, verticalInput) * speed * Time.fixedDeltaTime + rb.position;
-        rb.MovePosition(newPosition);
+        Vector2 velocity = new Vector2(horizontalInput, verticalInput) * speed * Time.fixedDeltaTime;
+        rb.velocity = velocity;
     }
 
     private void AimAtCursor()
@@ -41,5 +41,10 @@ public class PlayerMovement : MonoBehaviour
         Vector2 dir =  rb.position - (Vector2)Helper.Camera.ScreenToWorldPoint(Input.mousePosition);
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         rb.SetRotation(Quaternion.Euler(new Vector3(0,0,angle+90)));
+    }
+
+    public Rigidbody2D GetRigidbody2D()
+    {
+        return rb;
     }
 }
